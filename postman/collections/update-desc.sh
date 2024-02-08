0b682b94bf4b130ca9bc4d80bd72b830a4e8b3e2
laughingbiscuit <sean@laughingbiscuit.com>
2024-02-08 18:01:08 +0000

    properly capture newlines

--- postman/collections/update-desc.sh
+++ postman/collections/update-desc.sh
@@ -1,9 +1,4 @@
 #!/bin/sh 
 set -e
 
-while read line; do
-  DESC=$DESC"$line\n"
-done < Desc.md
-echo "$DESC"
-
-jq --arg desc "$DESC" '.info.description = $desc' PostmanEcho.json | sponge PostmanEcho.json
+jq --arg desc "$(cat Desc.md)" '.info.description = $desc' PostmanEcho.json | sponge PostmanEcho.json
